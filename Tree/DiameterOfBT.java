40be1b9e441b6a4718b4c6a37a2ca5b020249054
hemant <hemantmeenaofficial954@gmail.com>
2025-12-21 02:01:16 +0530

    add width of binary tree  problem code

---
+++ Tree/DiameterOfBT.java
@@ -0,0 +1,41 @@
+package Tree;
+
+public class DiameterOfBT {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static int diameter = 0;
+
+    static int height(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
+
+    static int getDiameter(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(getDiameter(root));
+    }
+}
+
