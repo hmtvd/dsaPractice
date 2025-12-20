1341c72e1e163b148a05ec528a873700838bdca2
hemant <hemantmeenaofficial954@gmail.com>
2025-12-21 01:55:46 +0530

    add height of binary tree problem code

---
+++ Tree/HeightOfBT.java
@@ -0,0 +1,28 @@
+package Tree;
+
+public class HeightOfBT {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(height(root));
+    }
+}
